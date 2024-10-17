1549018160f027d66936498d4bb72ae231417581
ElonaHoxhaj96 <elona6915@gmail.com>
2024-10-17 12:25:19 +0200

    feat: header with logo and cart

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,4 @@
+
 <script>
 export default{
     name: 'Header',
@@ -15,16 +16,37 @@ export default{
 </script>
 
 <template>
-   <div class="conatainer">
-    <div class="row">
-        <img src="" alt="">
+   <div class="container">
+        <div class="logo"><img src="../../public/FULL_LOGO.svg" alt=""></div>
+        <i class="fa-solid fa-cart-shopping icona" title="Carrello"></i>
     </div>
-   </div>
+   
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
 @use '../styles/partials/variables' as *;
 @use '../styles/partials/mixins' as *;
+
+.container{
+    background-color: $background_color;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .logo{
+        width: 200px;
+            img{
+                width: 100%;
+            }
+         
+    }
+
+    .icona{
+        font-size: 20px;
+        padding: 1rem;
+        color: $red;  
+    }
+}
+
 
 </style>
