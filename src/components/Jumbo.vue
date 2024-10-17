34aafeec191cfb064b8843b1effc44cf17222c7e
FraCuboni <fracuboni@gmail.com>
2024-10-17 12:25:47 +0200

    feat: animating jumbo

--- src/components/Jumbo.vue
+++ src/components/Jumbo.vue
@@ -15,11 +15,14 @@ export default{
 </script>
 
 <template>
-   <div class="container">
-    <div class="row">
-        <img src="" alt="">
+    <div class="container">
+        <img class="burger" src="../../public/burgir_svg.png" alt="">
+
+        <!-- animazione -->
+         <img class="slider" src="../../public/slider_deliveboo.svg" alt="">
+         <img class="slider" src="../../public/slider_deliveboo.svg" alt="">
+         <img class="slider" src="../../public/slider_deliveboo.svg" alt="">
     </div>
-   </div>
 </template>
 
 <style lang="scss" scoped>
@@ -29,6 +32,33 @@ export default{
 
 .container{
     background-color: $yellow;
+    min-height: 100px;
+    position: relative;
+
+    display: flex;
+    justify-content: center;
+
+    img.burger{
+        padding: 30px 0;
+        z-index: 2;
+    }
+    img.slider{
+        position: absolute;
+        animation: slide 7s linear infinite;
+        color: white;
+
+        @keyframes slide {
+            0% {
+                transform: translateX(200%);
+            }
+            50% {
+                transform: translateX(0);
+            }
+            100% {
+                transform: translateX(-200%);
+            }
+        }
+    }
 }
 
 </style>
