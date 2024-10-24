2d100329815f1d180bbeab3075f4c0bf111ac6fa
ElonaHoxhaj96 <elona6915@gmail.com>
2024-10-24 10:56:23 +0200

    feat: added prevent go back in the logo-link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,25 @@
-
 <script>
-export default{
+export default {
     name: 'Header',
-    components : {
-    },
-    data(){
-        return{
-
+    methods: {
+        goBack() {
+            window.history.back(); // Torna alla pagina precedente
         }
-    },
-    methods:{
-
     }
 }
 </script>
 
 <template>
-   <div class="container">
-        <div class="logo"><a href="/"><img src="../../public/FULL_LOGO.svg" alt=""></a></div>
-        <div class="buttons">    
-            <a href="http://127.0.0.1:8000/">Collabora con noi</a>
-            <a href=""><i class="fa-solid fa-cart-shopping icon" title="Carrello"></i></a>
-        </div>
-    </div>
-   
-</template>
+    <div class="container">
+         <div class="logo">
+             <a href="#" @click.prevent="goBack"><img src="../../public/FULL_LOGO.svg" alt=""></a>
+         </div>
+         <div class="buttons">    
+             <a href="http://127.0.0.1:8000/">Collabora con noi</a>
+             <a href=""><i class="fa-solid fa-cart-shopping icon" title="Carrello"></i></a>
+         </div>
+     </div>
+ </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
