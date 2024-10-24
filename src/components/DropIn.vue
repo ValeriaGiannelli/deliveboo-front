783038ae31a2bd31178b7fdc9c8debdf3e747d63
MicheleLafronza <michele.lafronza91@gmail.com>
2024-10-24 17:24:41 +0200

    feat: add logic for response

--- src/components/DropIn.vue
+++ src/components/DropIn.vue
@@ -1,20 +1,30 @@
 <template>
-    <div>
+    <h4 v-show="failed">Errore durante la transazione, riprova</h4>
+    <div v-if="cart_product.length > 0 && !paid && !initialize">
+      <div v-show="!paid" class="container">
+        <h2>Il totale del tuo ordine ammonta a {{ total_price }}&euro;</h2>
 
-      <h2>Il totale del tuo ordine ammonta a {{ total_price }}</h2>
+        <h3>Inserisci le informazioni e procedi al pagamento</h3>
 
-      <h3>Inserisci le tue informazioni e procedi al pagamento</h3>
-
-      <div class="container">
+        
         <input type="text" name="full_name" v-model="full_name" placeholder="Inserisci il tuo nome e cognome">
         <input type="email" name="email" v-model="email" placeholder="Inserisci la tua mail">
         <input type="text" name="address" v-model="address" placeholder="Inserisci il tuo indirizzo">
-        <input type="text" name="phone_number" v-model="phone_number" placeholder="Inserisci il tuo indirizzo">
+        <input type="text" name="phone_number" v-model="phone_number" placeholder="Inserisci il tuo numero di telefono">
       </div>
 
-      <div id="dropin-container"></div>
-      <button @click="submitPayment">Paga</button>
+      <div id="dropin-container" v-show="!paid"></div>
+      <p v-if="!paid">Effettua il pagamento</p>
+      <button v-show="!paid" @click="submitPayment" >Paga</button>
     </div>
+
+    <p v-else-if="cart_product.length === 0 && !paid && !initialize">Non hai ancora riempito il tuo carrello!</p>
+
+    <p v-else-if="initialize">Pagamento in corso</p>
+
+    <p v-else-if="!initialize">Pagamento effettuato con successo</p>
+
+    
   </template>
   
   <script>
@@ -31,7 +41,9 @@
         address : '',
         phone_number : '',
         total_price : '',
-        cart_product : []
+        cart_product : [],
+        initialize : false,
+        failed : false
       };
     },
     mounted() {
@@ -112,6 +124,7 @@
             console.error("Errore nella richiesta del metodo di pagamento:", err);
             return;
           } else {
+            this.initialize = true;
             // Invia il `payload.nonce` al server per processare il pagamento
             fetch("http://127.0.0.1:8000/api/orders/make/payment", {
               method: 'POST',
@@ -126,7 +139,10 @@
             .then(response => response.json())
             .then(data => {
               console.log("Risultato del pagamento:", data);
-              this.paid = true;
+              if(data.success){
+                this.paid = true;
+                this.failed = false;
+              }
               console.log('paid', this.paid);
                 
 
@@ -150,6 +166,11 @@
                 .then(response => response.json())
                 .then(data => {
                   console.log("Ordine andato a buon fine", data);
+                  localStorage.clear();
+                  this.cart_product = [];
+                  console.log(this.cart_product);
+                  this.initialize = false;
+                  
                 })
                 .catch(error => {
                   console.error("Errore durante la registrazione dell'ordine", error);
@@ -175,12 +196,19 @@
                 .catch(error => {
                   console.error("Errore durante invio mail", error);
                 }) */
+
+              // PAID FALSE 
+              } else {
+                this.failed = true;
+                this.initialize = false;
               }
+
             })
             .catch(error => {
               console.error("Errore durante la transazione:", error);
               this.paid = false;
             });
+
           }
         });
       },
