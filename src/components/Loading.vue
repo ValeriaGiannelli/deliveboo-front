fc865b4c2506d67e47c87ea12af98c32f7b0cd6c
FraCuboni <fracuboni@gmail.com>
2024-10-24 09:55:06 +0200

    feat: changed loading screen

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,7 +1,10 @@
 <script>
+import Loader from './partials/loader.vue';
+import loader from './partials/loader.vue';
 export default{
     name: 'Loading',
     components : {
+        Loader,
     },
     data(){
         return{
@@ -16,12 +19,7 @@ export default{
 
 <template>
     <div class="container">
-
-        <div>
-            <h1 class="loading-text">Caricamento<span class="dot">.</span><span class="dot">.</span><span class="dot">.</span></h1>
-            <img src="../../public/LOGO.svg" alt="">
-        </div>
-
+        <loader/>
     </div>
 </template>
 
@@ -39,54 +37,5 @@ export default{
     display: flex;
     justify-content: center;
     align-items: center;
-
-    div{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 50px;
-        h1{
-            font-size: 50px;
-            color: $yellow;
-            display: inline;
-            .dot{
-                color: $yellow;
-                opacity: 0;
-                animation: dot-blink 1.5s infinite;
-
-                &:nth-child(1){
-                    animation-delay: 0ms;
-                }
-                &:nth-child(2){
-                    animation-delay: 500ms;
-                }
-                &:nth-child(3){
-                    animation-delay: 1000ms;
-                }
-            }
-
-            @keyframes dot-blink {
-            0% { opacity: 0; }
-            33% { opacity: 1; }
-            66% { opacity: 1; }
-            100% { opacity: 0; }
-}
-
-        }
-        img{
-            animation: spin 3s linear infinite;
-        }
-
-    }
-
-    @keyframes spin {
-    0% {
-        transform: rotate(0deg);
-        }
-    100% {
-        transform: rotate(360deg);
-        }
-    }
 }
 </style>
